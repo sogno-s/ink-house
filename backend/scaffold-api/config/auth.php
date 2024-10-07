8b2d9441876273ee24af5d4f51255d6c51a8bf10
Елизавета Витальевна Соха <sev@lab-407-14.tomtit-tomsk.ru>
2024-10-07 15:06:49 +0700

    need fix

--- backend/scaffold-api/config/auth.php
+++ backend/scaffold-api/config/auth.php
@@ -36,8 +36,8 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
+        'api' => [
+            'driver' => 'token',
             'provider' => 'users',
         ],
     ],
@@ -64,11 +64,6 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
--- backend/scaffold-api/routes/api.php
+++ backend/scaffold-api/routes/api.php
@@ -4,8 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+
+//Route::get('/products', [ProductController::class, 'index']);
+
 Route::middleware('auth:api')->group(function(){
     Route::get('/products', [ProductController::class, 'index']);
     Route::post('/products', [ProductController::class, 'store'])->middleware('admin');
     Route::post('/products/upload', [ProductController::class, 'uploadImage']);
+
 });
+
