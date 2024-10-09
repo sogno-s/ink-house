de6d543fefaaed80ec943132ac036dc00641eda9
Елизавета Витальевна Соха <sev@lab-407-11.tomtit-tomsk.ru>
2024-10-09 14:00:22 +0700

    --

--- backend/scaffold-api/app/Http/Controllers/ProductController.php
+++ backend/scaffold-api/app/Http/Controllers/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use http\Env\Response;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Models\Product;
@@ -17,8 +18,11 @@ class ProductController extends Controller
             'author' => 'required|string|max:255',
             'description' => 'nullable|string',
             'price' => 'nullable|numeric',
+            'linkToImage'=> 'nullable|string|max:255',
+            'category_id'=>'nullable|exist:category,id',
         ]);
         $product = Product::create($request->all());
+
         return response()->json($product, 201);
     }
 
@@ -29,5 +33,23 @@ class ProductController extends Controller
         $path = $request->file('image')->store('images');
 
         return response()->json(['path'=>$path], 201);
+    }
+
+    public function destroy($id){
+        $product = Product::find($id);
+        if (!$product){
+            return response()->json(['message'=>'Product not found'], 404);
+        }
+        $product->delete();
+
+        return response()->json(['message'=>'Product delete successfully']. 200);
+    }
+
+    public function show($id){
+        $product = Product::with('category')->find($id);
+        if (!$product){
+            return response()->json(['message'=>'Product not found'],404);
+        }
+        return response()->json($product, 200);
     }
 }
